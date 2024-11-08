f6a09db5622e4fb8d0a61ae02c0f7caa6630755f
Peter Lesiak <piolesiak12@gmail.com>
2024-11-08 23:21:32 +0100

    refactor: setup

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+    arrowParens: 'avoid',
+    experimentalTernaries: true,
+    printWidth: 95,
+    tabWidth: 4,
+    singleQuote: true,
+};
+
+export default config;
