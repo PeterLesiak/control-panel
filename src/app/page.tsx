ceb153872eceebe0d6aa53fd53b0d9789c1f89dc
Peter Lesiak <piolesiak12@gmail.com>
2024-11-08 23:22:13 +0100

    feat: link to github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <>
             <h1 className="text-center text-2xl p-4 border-b bg-slate-900">
-                Control Panel v1.0.0
+                <a href="http://github.com/PeterLesiak/control-panel">Control Panel v1.0.0</a>
             </h1>
         </>
     );
